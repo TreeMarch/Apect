a36f4903c139485773a7a6bc501030af99157e63
Tran Trong Trinh <m5c0traan@gmail.com>
2023-09-15 21:30:29 +0700

    d4_scanf remove float and pratice changes

--- d4_scanf.c
+++ d4_scanf.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-int d4_scanf(){
-//    int main(){
+//int d4_scanf(){
+    int main(){
     int a;
-    float d =123.4545;
-    printf("float 123.4545 : %e",d);
+//    float d =123.4545;
+//    printf("float 123.4545 : %e",d);
     char fullName[100];
+    printf("moi ban nhap ten:");
     gets(fullName);
-    printf("fullName : %s \n",fullName);
+    printf("fullName: %s \n",fullName);
     char ch , name[40];
     printf("please enter the data : \n");
-    scanf("%d %f %c %s",&a,&d,&ch,name);
-    printf("\n the value accepted are : %d,%f ,%c ,%s",a,d,ch,name);
+    scanf("%d %f %c %s",&a,&ch,name);
+    printf("\n the value accepted are : %d,%f ,%c ,%s",a,ch,name);
     return 0;
 
 
-//    }
-}
+    }
+//}
---
+++ practice_num1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int practice(){
+//    int main(){
+        int a,b,c,d,i;
+        int r1,r2,r3;
+        a = 6;
+        b = 4;
+        c = 10;
+        d = 5;
+        i =12;
+        r1 = ++i % 7;
+        printf("  r1 =%d \n", i,r1);
+        r2 = 5*(c- 3 + d);
+        printf("  r2 = %d \n ",c,d,r2);
+        r3 = a *(b + c/d) - 22;
+        printf(" r3 = %d",a,b,c,d,r3);
+//    }
+}
